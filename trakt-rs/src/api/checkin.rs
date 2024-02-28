5ba75806726262e1fee13ca6d51a69fdd578c97f
Anshul Gupta <ansg191@anshulg.com>
2024-02-28 02:37:56 -0800

    perf: reduces allocations in checkin endpoint

--- trakt-rs/src/api/checkin.rs
+++ trakt-rs/src/api/checkin.rs
@@ -10,10 +10,10 @@ pub mod checkin {
 
     use bytes::BufMut;
     use serde::Deserialize;
-    use serde_json::{json, Value};
     use time::OffsetDateTime;
     use trakt_core::{error::IntoHttpError, AuthRequirement, Context, Metadata};
 
+    use self::_private::CheckinItemType;
     use crate::smo::{Episode, Id, Ids, Movie, Sharing, Show};
 
     #[derive(Debug, Clone, Eq, PartialEq, Hash)]
@@ -65,21 +65,38 @@ pub mod checkin {
             self,
             ctx: Context,
         ) -> Result<http::Request<T>, IntoHttpError> {
+            #[derive(Debug, serde::Serialize)]
+            #[serde(rename_all = "lowercase")]
+            enum BodyInner {
+                Movie { ids: Ids },
+                Episode { ids: Ids },
+            }
+
+            #[derive(Debug, serde::Serialize)]
+            struct Body {
+                #[serde(flatten)]
+                item: BodyInner,
+                #[serde(skip_serializing_if = "Option::is_none")]
+                sharing: Option<Sharing>,
+                #[serde(skip_serializing_if = "Option::is_none")]
+                message: Option<String>,
+            }
+
             let body = T::default();
             let mut writer = body.writer();
 
-            let json = Value::Object({
-                let mut map = serde_json::Map::new();
-                map.insert(I::KEY.to_owned(), json!({ "ids": Ids::from(self.id) }));
-                if let Some(sharing) = self.sharing {
-                    map.insert("sharing".to_owned(), json!(sharing));
-                }
-                if let Some(message) = self.message {
-                    map.insert("message".to_owned(), json!(message));
-                }
-                map
-            });
-
+            let json = Body {
+                item: match I::TYPE {
+                    CheckinItemType::Movie => BodyInner::Movie {
+                        ids: self.id.into(),
+                    },
+                    CheckinItemType::Episode => BodyInner::Episode {
+                        ids: self.id.into(),
+                    },
+                },
+                sharing: self.sharing,
+                message: self.message,
+            };
             serde_json::to_writer(&mut writer, &json)?;
 
             trakt_core::construct_req(&ctx, &Self::METADATA, &(), &(), writer.into_inner())
@@ -90,15 +107,21 @@ pub mod checkin {
         use crate::smo::{Episode, Movie};
 
         pub trait Sealed {
-            const KEY: &'static str;
+            const TYPE: CheckinItemType;
+        }
+
+        #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+        pub enum CheckinItemType {
+            Movie,
+            Episode,
         }
 
         impl Sealed for Movie {
-            const KEY: &'static str = "movie";
+            const TYPE: CheckinItemType = CheckinItemType::Movie;
         }
 
         impl Sealed for Episode {
-            const KEY: &'static str = "episode";
+            const TYPE: CheckinItemType = CheckinItemType::Episode;
         }
     }
 
