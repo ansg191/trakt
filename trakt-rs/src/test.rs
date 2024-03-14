478272f94e64cd94e900c829912e64b925faea5b
Anshul Gupta <ansg191@anshulg.com>
2024-03-13 19:31:30 -0700

    Revert "Upgrades to http 1"

--- trakt-rs/src/test.rs
+++ trakt-rs/src/test.rs
@@ -1,7 +1,13 @@
+use std::sync::OnceLock;
+
+use bytes::Bytes;
+use reqwest::blocking::Client;
 use trakt_core::{
     error::{FromHttpError, IntoHttpError},
     Context, Request, Response,
 };
+
+static CLIENT: OnceLock<Client> = OnceLock::new();
 
 pub fn assert_request<R, T>(ctx: Context, req: R, exp_url: &str, exp_body: &T)
 where
@@ -35,26 +41,43 @@ where
 }
 
 pub fn execute<R: Request>(ctx: Context, req: R) -> Result<R::Response, Error> {
-    let request: http::Request<Vec<u8>> = req.try_into_http_request(ctx)?;
-    let (parts, body) = request.into_parts();
-    let request = ureq::Request::from(parts);
+    let client = CLIENT.get_or_init(Client::new);
 
-    let response = request.send_bytes(&body)?;
-    let http_res: http::Response<Vec<u8>> = http::Response::from(response);
+    let request: http::Request<Vec<u8>> = req.try_into_http_request(ctx)?;
+
+    let reqwest_res = client.execute(request.try_into()?)?;
+    let http_res = reqwest_to_http(reqwest_res)?;
 
     Ok(Response::try_from_http_response(http_res)?)
 }
 
+fn reqwest_to_http(
+    mut response: reqwest::blocking::Response,
+) -> Result<http::Response<Bytes>, Error> {
+    let mut builder = http::Response::builder()
+        .status(response.status())
+        .version(response.version());
+
+    std::mem::swap(
+        builder.headers_mut().expect("response should be valid"),
+        response.headers_mut(),
+    );
+
+    Ok(builder
+        .body(response.bytes()?)
+        .expect("response should be valid"))
+}
+
 #[derive(Debug)]
 pub enum Error {
-    Reqwest(Box<ureq::Error>),
+    Reqwest(reqwest::Error),
     IntoHttp(IntoHttpError),
     FromHttp(FromHttpError),
 }
 
-impl From<ureq::Error> for Error {
-    fn from(e: ureq::Error) -> Self {
-        Self::Reqwest(Box::new(e))
+impl From<reqwest::Error> for Error {
+    fn from(e: reqwest::Error) -> Self {
+        Self::Reqwest(e)
     }
 }
 
